f30d1643c89ba7834da6bb2359d284ed04feb229
Kiriakos <l1skou@gmail.com>
2019-04-01 21:51:15 +0300

    TD added, BB misc scene a2

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+
+    int currentSceneIndex;
+    [SerializeField] float timeToLoad = 4f;
+
+    void Start()
+    {
+        currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        if (currentSceneIndex == 0)
+        {
+            StartCoroutine(WaitToLoad());
+        }
+    }
+
+    IEnumerator WaitToLoad()
+    {
+        yield return new WaitForSeconds(timeToLoad);
+        LoadNextScene();
+    }
+
+    public void LoadNextScene()
+    {
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
