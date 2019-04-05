49272aa805fa739fa562da708dcf40ce661b9124
Kiriakos <l1skou@gmail.com>
2019-04-05 15:42:58 +0300

    TD GO conditions, polishing

---
+++ Assets/Scripts/GameOver.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameOver : MonoBehaviour
+{
+
+    [SerializeField] Text text;
+
+    void Start()
+    {
+        text.CrossFadeAlpha(255f, 5f, false);
+    }
+
+
+    void Update()
+    {
+
+    }
+}
